4f95cffe77a45ebde311d6a9efc6619ed8e00761
Seafire89 <piect89gmail.com>
2025-06-20 11:35:12 +0200

    inizializzazione

---
+++ ProgettoCalcolatrice/src/calcolatrice/Calcolatrice.java
@@ -0,0 +1,24 @@
+package calcolatrice;
+
+public class Calcolatrice {
+
+    // Somma di due interi
+    public int somma(int a, int b) {
+        return a + b;
+    }
+
+    // Somma di tre interi
+    public int somma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Somma di due double
+    public double somma(double a, double b) {
+        return a + b;
+    }
+
+    // Concatenazione di due stringhe
+    public String somma(String a, String b) {
+        return a + " + " + b;
+    }
+}
